93c437c1dcaa16f1d320af7289c6e953d8b60c7f
Vlad BENCHEA <bencheavld@gmail.com>
2021-04-02 17:46:08 +0300

    Removed unused imports

--- CatanAPI/CatanAPI/Models/Contacts.cs
+++ CatanAPI/CatanAPI/Models/Contacts.cs
@@ -1,6 +1,3 @@
-using System.Collections.Generic;
-using Microsoft.EntityFrameworkCore;
-
 namespace CatanAPI.Models
 {
     public class Contact
--- CatanAPI/CatanAPI/Models/User.cs
+++ CatanAPI/CatanAPI/Models/User.cs
@@ -1,5 +1,3 @@
-using System.Collections.Generic;
-using Microsoft.EntityFrameworkCore;
 
 namespace CatanAPI.Models
 {
