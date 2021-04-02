865a2e21bf81136e7819b220ef85343f5baf2337
Vlad BENCHEA <bencheavld@gmail.com>
2021-04-02 17:44:37 +0300

    Added Contact model

---
+++ CatanAPI/CatanAPI/Models/Contacts.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatanAPI.Models
+{
+    public class Contact
+    {
+        public int ContactId { get; set; }
+        public bool Accepted { get; set; }
+        public User Sender { get; set; }
+        public User Receiver { get; set; }
+        public int SenderId { get; set; }
+        public int ReceiverId { get; set; }
+    }
+}
