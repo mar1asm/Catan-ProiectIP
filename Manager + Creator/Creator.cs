d02a5f5699f1ed78d112f74f9ef9a2bbe95c7f5c
Claudyou <59448503+Claudyou@users.noreply.github.com>
2021-04-16 19:45:19 +0300

    Add files via upload

---
+++ Manager + Creator/Creator.cs
@@ -0,0 +1,3 @@
+interface Creator{
+    string toString();
+}
---
+++ Manager + Creator/Manager.cs
@@ -0,0 +1,117 @@
+
+
+public sealed class Manager//Patronu'
+{
+
+    private RulesExpansion rules = new RulesExpansion();
+    private BoardExpansion board= new BoardExpansion();
+    private List<SettlementExpansion> settlements= new List<SettlementExpansion>();
+    private List<ConnectorCreator> connectors = new List<ConnectorCreator>();
+    private List<CardExpansion> cards= new List<CardExpansion>();
+    private List<DeckExpansion> decks= new List<DeckExpansion>();
+    private List<TileExpansion> tiles= new List<TileExpansion>();
+
+
+    Manager(){
+    }
+
+    private static readonly object padlock= new object();
+    private static Manager instance = null;
+    public static Manager  Instance {
+        get{
+            if(instance == null){
+                lock(padlock){
+                    instance = new Manager();
+                }
+            }
+            return instance;
+        }
+    }
+
+    
+    // private List<string> _TheList = new List<string>();
+    // public List<string> TheList
+    // {
+    //     get { return _TheList; }
+    //     set { _TheList = value; }
+    // }
+    
+    public RulesExpansion Rules{
+        get { return rules;}
+        set { rules = value;}
+    }
+
+    public BoardExpansion Board{
+        get { return board;}
+        set { board = value;}
+    }    
+    
+    public  List<SettlementExpansion> Settlements{
+        get { return settlements;}
+        set { settlements = value;}
+    }
+    
+    List<ConnectorCreator> Connectors{
+        get { return connectors; }
+        set { connectors = value; }
+    }
+         
+    public List<TileExpansion> Tiles{
+        get{ return tiles;}
+        set{ tiles = value;}
+    }
+    public List<DeckExpansion> Decks{
+        get{ return decks;}
+        set{ decks = value;}
+    }
+
+    public List<CardExtension> Cards{
+        get{ return cards;}
+        set{ cards = value;}
+    }
+
+
+    public string toStringAvailableTiles(){
+        string output = "\"availableTiles\": [";
+
+        // {
+        //     "type": "sheep",
+        //     "number": 4
+        // }
+     foreach(Tiles currentTile in tileList)
+     {
+        if(output.IndexOf(output, 0, output.Length()) == '}'){
+            output += ",\n";
+        }
+        else
+            output += "\n";
+        output += "{\n";
+
+        output += "\"type\": " + "\"" + currentTile.type +"\",";
+
+        output += "\"number\": " + nrOfTiles[currentTile] + "\n";
+
+        output += "}";
+
+
+     }
+
+     output += "]";
+
+     return output;
+    }
+
+    public void toFile(){
+        
+
+        // scriem in fisier ce returneaza`toStringAvailableTiles`
+        string output = "";
+        output = "{\n";
+        output += toStringAvailableTiles() + ",\n";
+        output += board.toString();
+        output += "\n}";
+        File.WriteAllText("board.txt", output);
+
+
+    }
+}
