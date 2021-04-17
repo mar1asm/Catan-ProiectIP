038babb9f2b195bedb7f78dacfd86108b3c8634d
Vlad BENCHEA <bencheavld@gmail.com>
2021-04-17 16:39:49 +0300

    refactorized Contact model

--- CatanAPI/CatanAPI/Models/Contact.cs
+++ CatanAPI/CatanAPI/Models/Contact.cs
@@ -2,13 +2,13 @@ namespace CatanAPI.Models
 {
     public class Contact
     {
-        public int ContactId { get; set; }
+        public int Id { get; set; }
         public bool Accepted { get; set; }
 
         public int SenderId { get; set; }
         public User Sender { get; set; }
 
-        public User Receiver { get; set; }
         public int ReceiverId { get; set; }
+        public User Receiver { get; set; }
     }
 }
