a91578cfb9c46b0b94d44d5890b03265ac85274a
Vlad BENCHEA <bencheavld@gmail.com>
2021-04-18 13:48:44 +0300

    updated Contact model

--- CatanAPI/CatanAPI/Models/Contact.cs
+++ CatanAPI/CatanAPI/Models/Contact.cs
@@ -5,10 +5,20 @@ namespace CatanAPI.Models
         public int Id { get; set; }
         public bool Accepted { get; set; }
 
-        public int SenderId { get; set; }
-        public User Sender { get; set; }
+        public string SenderId;
+        public virtual User Sender { get; set; }
 
-        public int ReceiverId { get; set; }
-        public User Receiver { get; set; }
+        public string ReceiverId;
+        public virtual User Receiver { get; set; }
+        public void AddContactRequest(User user, User friendUser)
+        {
+            var friendRequest = new Contact()
+            {
+                Sender = user,
+                Receiver = friendUser,
+                Accepted = false
+            };
+            user.SentContactRequests.Add(friendRequest);
+        }
     }
 }
