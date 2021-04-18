232d722c703f68de9bbb26751a5cc1062f7f3aa2
Vlad BENCHEA <bencheavld@gmail.com>
2021-04-18 14:48:59 +0300

    renamed Id field in UserDto

--- CatanAPI/CatanAPI/Data/DTO/UserDto.cs
+++ CatanAPI/CatanAPI/Data/DTO/UserDto.cs
@@ -4,7 +4,7 @@ namespace CatanAPI.Data.DTO
 {
     public class UserDto
     {
-        public string UserId { get; set; }
+        public string Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
