ef7884597f0e1834d2bb4963c689ace2d3c9fa55
IoanPuiu <office.ionutz@gmail.com>
2021-04-18 17:27:58 +0300

    place connector; place settlement; get list of available corners, get list of available connectors
    class Connector and and classes Road and Boat which inherit Connector;
    classes ConnectorBehaviour and SettlementBehaviour which inherit MonoBehaviour (created having as start exeple TaleBehaviour);
    methods in BoardManagerBehaviour:
    -AddConnector
    -AddSettlement
    -AddTile
    - settlementPrefab and ConnectorPrefab in Unity;
    methos in Board:
    - placeConnector
    - placeSettlement
    -  GetAvailableCorners
    - GetAvailableConnectors
    - GetSettlementFromString
    - GetConnectorFromString

--- CatanPrototype/Assets/Scripts/GameLogic/BoardManager/Board.cs
+++ CatanPrototype/Assets/Scripts/GameLogic/BoardManager/Board.cs
@@ -37,6 +37,8 @@ public class Board
         {
             playerRoadNetworks[i] = new Dictionary<BoardCoordinate, List<BoardCoordinate>>();
         }
+
+
     }
 
     /// <summary>
@@ -45,11 +47,12 @@ public class Board
     /// <param name="color"></param>
     /// <param name="corner1"></param>
     /// <param name="corner2"></param>
-    public void PlaceConnector(PlayerColor color, Corner corner1, Corner corner2)
+    //---------------------------------------DE SCHIMBAT NUMELE LA FCT ASTA !!! -----------------------------
+    public void PlaceConnectorSCHIMBA(PlayerColor color, Corner corner1, Corner corner2)
     {
         BoardCoordinate bc1 = corner1.coordinate;
         BoardCoordinate bc2 = corner2.coordinate;
-        PlaceConnector(color, bc1, bc2);
+        PlaceConnectorSCHIMBA(color, bc1, bc2);
     }
     /// <summary>
     /// Functia care adauga un conector intre doua puncte de pe harta
@@ -59,7 +62,8 @@ public class Board
     /// <param name="color"></param>
     /// <param name="bc1"></param>
     /// <param name="bc2"></param>
-    private void PlaceConnector(PlayerColor color, BoardCoordinate bc1, BoardCoordinate bc2)
+    //---------------------------------------DE SCHIMBAT NUMELE LA FCT ASTA !!! -----------------------------
+    private void PlaceConnectorSCHIMBA(PlayerColor color, BoardCoordinate bc1, BoardCoordinate bc2)
     {
         int networkIndex = (int)color;
         var network = playerRoadNetworks[networkIndex];
@@ -87,7 +91,9 @@ public class Board
         {
             network[bc2].Add(bc1);
         }
+        //aici nu trebuie pus network in playerRoadNetworks[networkIndex]?? ? ? ? ? ?  ? ? ?
     }
+    
     private string ExtractRandomTileType()
     {
         int index = Random.Range(0, availableTileTypes.Count);
@@ -132,9 +138,116 @@ public class Board
 
         tiles.Add(boardCoordinate, tileToPlace);
 
-
         return tileToPlace;
     }
+
+    public List<Corner> GetAvailableCorners(PlayerColor color)
+    {
+        //made by jon
+        List<Corner> listOfCorners= new List<Corner>();
+        foreach (var it in playerRoadNetworks[(int)color])
+        {
+            bool ok = true;
+            BoardCoordinate bc=it.Key;
+            List<BoardCoordinate> bclist= cornerLattice[bc];
+            foreach (var b in bclist)
+                if (corners[b].settlement != null) // daca este asezare in vecini atunci "bc" nu este disponibil
+                    ok = false;
+            if(ok) listOfCorners.Add(corners[bc]);
+        }
+
+        return listOfCorners;
+    }
+
+    public List<KeyValuePair<Corner,Corner>> GetAvailableConnectors(PlayerColor color)
+    {
+        List<KeyValuePair<Corner, Corner>> listOfConnectors = new List<KeyValuePair<Corner, Corner>>();
+
+        foreach (var it in playerRoadNetworks[(int)color])
+        {
+            bool ok = true;
+            BoardCoordinate bc = it.Key; //bc este una dintre coordonate
+            List<BoardCoordinate> bclist = cornerLattice[bc];//list de vcini ai lui bc
+            foreach (var b in bclist)
+            {
+                for (int i = 0; i < (int)PlayerColor.NbOfColors; i++)
+                    if (playerRoadNetworks[i].ContainsKey(b)) //daca un vecin face parte din graful unui player
+                        if (playerRoadNetworks[i].ContainsKey(bc)) // si bc face si l part din graful aceluias player 
+                            ok = false;// perechea (b,bc) nu este buna pt ca acolo deja exista un drum
+                if(ok)
+                {
+                    KeyValuePair<Corner, Corner> item = new KeyValuePair<Corner, Corner>(corners[b], corners[bc]);
+                    listOfConnectors.Add(item);
+                }
+            }
+        }
+
+        return listOfConnectors;
+
+    }
+
+    public Settlement PlaceSettlement(Player p, BoardCoordinate boardCoordinate, string type)
+    {
+        //made by jon
+        
+        int colorID = (int)p.color;
+        Settlement settlementToPlace = GetSettlementFromString(boardCoordinate, type);
+
+        if (playerRoadNetworks[colorID].ContainsKey(boardCoordinate))
+        {
+            List<Corner> availableCorners = GetAvailableCorners(p.color);
+            foreach (var c in availableCorners)
+                if(boardCoordinate==c.coordinate)
+                    return settlementToPlace;
+        }
+
+        return null;
+    }
+
+    public Connector PlaceConnector(Player p, BoardCoordinate bc1, BoardCoordinate bc2, string type)
+    {
+        //made by jon
+
+        int colorID = (int)p.color;
+        Connector connectorToPlace = GetConnectorFromString(bc1, bc2, type);
+
+        if (playerRoadNetworks[colorID].ContainsKey(bc1) || playerRoadNetworks[colorID].ContainsKey(bc2))// daca macar una din cele doua coordonate face parte din graful playerului
+        {
+            List<KeyValuePair<Corner, Corner>> availableConnectors = GetAvailableConnectors(p.color);
+            foreach(var conn in availableConnectors)
+            {
+                if (bc1 == conn.Key.coordinate && bc2 == conn.Value.coordinate ||
+                    bc2 == conn.Key.coordinate && bc1 == conn.Value.coordinate)
+                    return connectorToPlace;//daca coordonatele se gasesc in lista availableConnectors atunci putem pune drumul
+            }
+        }
+
+        return null;
+
+
+        /*
+        if(playerRoadNetworks[colorID].ContainsKey(bc1)&& !playerRoadNetworks[colorID].ContainsKey(bc2))
+        {
+            for(int i=0; i<(int)PlayerColor.NbOfColors;++i)
+            {
+                if (i == colorID) continue;
+                if(playerRoadNetworks[i].ContainsKey(bc2)) return null;
+            }
+            return connectorToPlace;
+        }
+
+        if (playerRoadNetworks[colorID].ContainsKey(bc2) && !playerRoadNetworks[colorID].ContainsKey(bc1))
+        {
+            for (int i = 0; i < (int)PlayerColor.NbOfColors; ++i)
+            {
+                if (i == colorID) continue;
+                if (playerRoadNetworks[i].ContainsKey(bc1)) return null;
+            }
+            return connectorToPlace;
+        }
+        return null;*/
+    }
+
 
     /*
      * Function that creates and adds the corners coresponding to a Tile
@@ -198,6 +311,54 @@ public class Board
             default: return new TestTile(boardCoordinate);
         }
     }
-    
+    private Settlement GetSettlementFromString(BoardCoordinate boardCoordinate, string type)
+    {
+        //made by jon
+        switch (type)
+        {
+            case "village": 
+                {
+                    Corner corner=new Corner(boardCoordinate); 
+                    return new Village(corner);
+                }
+            case "city": 
+                {
+                    Corner corner = new Corner(boardCoordinate); 
+                    return new City(corner);
+                }
+            default:
+                {
+                    Corner corner = new Corner(boardCoordinate); 
+                    return new TestSettlement(corner);
+                }
+        }
+    }
+
+    private Connector GetConnectorFromString(BoardCoordinate bc1, BoardCoordinate bc2, string type)
+    {
+        //made by jon
+        switch (type)
+        {
+            case "road":
+                {
+                    Corner c1 = new Corner(bc1);
+                    Corner c2 = new Corner(bc2);
+                    return new Road(c1,c2);
+                }
+            case "boat":
+                {
+                    Corner c1 = new Corner(bc1);
+                    Corner c2 = new Corner(bc2);
+                    return new Boat(c1, c2);
+                }
+            default:
+                {
+                    Corner c1 = new Corner(bc1);
+                    Corner c2 = new Corner(bc2);
+                    return new TestConnector(c1,c2);
+                }
+        }
+    }
+
 
 }
