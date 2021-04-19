8900388b25f8c37b5f948850229bc9a1698ed874
cristi0995 <bujoreanucristian19@gmail.com>
2021-04-19 14:10:45 +0300

    Update Player.cs

--- CatanPrototype/Assets/Scripts/GameLogic/PlayerManager/Player.cs
+++ CatanPrototype/Assets/Scripts/GameLogic/PlayerManager/Player.cs
@@ -8,4 +8,16 @@ public class Player
     public string nickname;
     public string ID;
     public PlayerColor color;
+    int score;
+    //DeckPlayer deck;
+
+    public void ScoreAdd(int value)
+    {
+        score = score + value;
+    }
+
+    public void ScoreSet(int value)
+    {
+        score = value;
+    }
 }
