d630db8e50b16d04cd54bacd500542aaabf605d5
Emanuel-G-H <71083373+Emanuel-G-H@users.noreply.github.com>
2021-04-20 08:03:33 +0300

    Add files via upload

---
+++ NewExtension.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ExtendClass
+{
+    class NewExtension
+    {
+        string path = "Path to the place where the data is stored.";
+        string pathListExtension = "Path to the place where te data is stored";
+        void sendToStore(string newPath)
+        {
+            path = newPath;
+        }
+        
+        string getFromStore()
+        {
+            return path;
+        }
+
+        string getExtentionList(){
+             return pathListExtension;
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
