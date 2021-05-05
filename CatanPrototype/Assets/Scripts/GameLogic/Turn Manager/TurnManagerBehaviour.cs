b3874870eccb45d54160e694bdc30fe686f774f0
alexxandra21 <76692842+alexxandra21@users.noreply.github.com>
2021-05-05 15:40:45 +0300

    Tested version of Turn Manager

--- CatanPrototype/Assets/Scripts/GameLogic/Turn Manager/TurnManagerBehaviour.cs
+++ CatanPrototype/Assets/Scripts/GameLogic/Turn Manager/TurnManagerBehaviour.cs
@@ -11,7 +11,22 @@ public class TurnManagerBehaviour : MonoBehaviour
     public int currentPlayer =-1;
    
     void Start()
-    {        
+    {
+        p = new Player[3];
+        size = 3;
+        p[0] = new Player();
+        p[1]= new Player();
+        p[2] = new Player();
+        p[0].nickname = "Ana";
+        p[1].nickname = "Ion";
+        p[2].nickname = "Alex";
+        p[0].ID = "12A";
+        p[1].ID = "12B";
+        p[2].ID = "12C";
+        SetOrder(p);//seteaza ordinea random a jucatorilor
+        TurnLogic(Next()); //face o miscare (aruncarea zarului si distribuitul resurselor) pentru urmatorul jucator
+      
+
     }
     //da cu zarul random de la 1 la 7
     public int RollDice()
@@ -43,27 +58,14 @@ public class TurnManagerBehaviour : MonoBehaviour
     public void TurnLogic(int x)
     {
         
-        Debug.Log("Randul lui" + p[x].nickname );
+        Debug.Log("Randul lui " + p[x].nickname );
         int dice1,dice2,number;
         //momentan p[x] face mutarea
         dice1 = RollDice();
         dice2=RollDice();
         number = dice1 + dice2; //Zarul 1 +Zarul 2     
-        number = 5;
-       // GiveResources(number);//jucatorii primesc resursele daca au asezari pe regiunea cu numarul picat
-    }
-    
-   
-    public void GiveResources(int nr)
-    {
-        Debug.Log("Jucatorii care au asezari pe" + nr + "primesc resurse.");
-        
-       /* foreach (KeyValuePair<BoardCoordinate, Tile> entry in board.tiles)
-        {
-            if (entry.Value.tile.number == nr && entry.Value is ResourceTypes)
-                tile.SpecialAction();       
-        }*/
-
+        Debug.Log(p[x].nickname + " a dat " + number+ " .");
+       //GiveResources(number);//jucatorii primesc resursele daca au asezari pe regiunea cu numarul picat
     }
    
     //returneaza numarul jucatorului care urmeaza 
