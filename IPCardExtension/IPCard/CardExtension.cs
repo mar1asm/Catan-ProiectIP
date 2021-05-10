7b2ab4378f44f692895ee851e0985f4daf7eb382
Dinco Ionut <75804600+ionutdinco@users.noreply.github.com>
2021-05-10 16:55:19 +0300

    Add files via upload

---
+++ IPCardExtension/IPCard/CardExtension.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IPCard
+{
+    class CardExtension
+    {
+        private int number;
+        private Byte[] image;
+        private String type;
+        private String description;
+
+
+        public String Type { get => type; set => type = value; }
+        public String Description { get => description; set => description = value; }
+
+        public Byte[] Image { get => image; set => image = value; }
+
+        public int Number { get => number; set => number = value; }
+
+        public string toString()
+        {
+            String config = "{\n";
+            config += "\"type\": " + type + ",\n";
+            config += "\"number\": " + number + "\n";
+            config += "}";
+
+            return config;
+        }
+}
+}
---
+++ IPCardExtension/IPCard/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace IPCard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CardExtension card = new CardExtension();
+            card.Type="sheep";
+            card.Number = 10;
+
+            CardExtension card2 = new CardExtension();
+            card2.Type = "wheat";
+            card2.Number = 10;
+
+            Console.WriteLine(card.toString());
+            Console.WriteLine(card2.toString());
+        }
+    }
+}
