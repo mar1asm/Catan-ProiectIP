4a7a4725e29ae82932b1f51e71634c043e7d46b6
EmilianDiac <71902443+EmilianDiac@users.noreply.github.com>
2021-05-10 20:10:40 +0300

    Add files via upload
    Implemented deck extention

---
+++ DeckExtention/DeckExtention.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeckExtention
+{
+    public class DeckExtention : Creator
+    {
+        public String Name { get; set; }
+        public List<CardExtension> CardExtentionList;
+        public DeckExtention(String name)
+        {
+            CardExtentionList = new List<CardExtension>();
+            this.Name = name;
+        }
+
+        public void addCards(CardExtension cardExtention) => CardExtentionList.Add(cardExtention);
+
+        public string toString()
+        {
+            String objectString = "{\n";
+            objectString += "\"name\": " + Name +",\n";
+            objectString += "\"d\": \n[\n";
+            foreach(var card in CardExtentionList)
+            {
+                objectString += card.toString() + ",\n";
+            }
+            objectString = objectString.Remove(objectString.Length - 2);
+ 
+            objectString += "\n]";
+            objectString += "\n}";
+
+            return objectString;
+        }
+    }
+}
---
+++ DeckExtention/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DeckExtention
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var newDeck = new DeckExtention("Test");
+
+            CardExtension card = new CardExtension();
+            card.Type = "sheep";
+            card.Number = 10;
+
+            CardExtension card2 = new CardExtension();
+            card2.Type = "wheat";
+            card2.Number = 10;
+
+            newDeck.addCards(card);
+            newDeck.addCards(card2);
+
+            Console.WriteLine(newDeck.toString());
+        }
+    }
+}
