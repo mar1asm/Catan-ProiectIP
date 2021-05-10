c38721a1161c627a711a19610813da2281be5f58
Vlad BENCHEA <bencheavld@gmail.com>
2021-05-11 00:06:47 +0300

    ExtensionController and support for get, get detailed, create and delete a game session

--- CatanAPI/CatanAPI/Controllers/GameSessionsController.cs
+++ CatanAPI/CatanAPI/Controllers/GameSessionsController.cs
@@ -3,19 +3,27 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using CatanAPI.Data;
-using CatanAPI.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Authorization;
 
+using CatanAPI.Data;
+using CatanAPI.Models;
+using CatanAPI.Data.DTO.GameSessionsDTO;
+using CatanAPI.Data.DTO.ExtensionsDTO;
+using CatanAPI.Data.DTO.UsersDTO;
+using System;
+
 namespace CatanAPI.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class GameSessionsController : ControllerBase
     {
         private readonly CatanAPIDbContext _context;
         private readonly UserManager<User> _userManager;
+
+        public DateTime Datetime { get; private set; }
 
         public GameSessionsController(CatanAPIDbContext context, UserManager<User> manager)
         {
@@ -24,67 +32,75 @@ namespace CatanAPI.Controllers
         }
 
         // GET: api/GameSessions
-        [Authorize]
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<GameSession>>> GetGameSessions()
+        public async Task<ActionResult<IEnumerable<GetSessionMinDto>>> GetGameSessions()
         {
-            return await _context.GameSessions.ToListAsync();
+            var sessions = await _context.GameSessions.Select(session => new GetSessionMinDto
+            {
+                Id = session.Id,
+                CreatedAt = session.CreatedAt,
+                Status = session.Status
+
+            }).ToListAsync();
+            return Ok(sessions);
         }
 
         // GET: api/GameSessions/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<GameSession>> GetGameSession(int id)
+        public async Task<ActionResult<GetSessionDto>> GetGameSession(int id)
         {
-            var gameSession = await _context.GameSessions.FindAsync(id);
+            var currentUser = await _userManager.FindByNameAsync(HttpContext.User.Identity.Name);
+            var gameSession = await _context.GameSessions
+                .Include(i => i.GameSessionUsers)
+                .Include(i => i.Extensions)
+                .FirstOrDefaultAsync(item => item.Id == id);
 
-            if (gameSession == null)
+            if (gameSession == null || gameSession.GameSessionUsers == null)
             {
                 return NotFound();
             }
-
-            return gameSession;
-        }
-
-        // PUT: api/GameSessions/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutGameSession(int id, GameSession gameSession)
-        {
-            if (id != gameSession.Id)
+            if(!gameSession.GameSessionUsers.Any(item => item.User.Id == currentUser.Id))
             {
-                return BadRequest();
+                return Unauthorized();
             }
 
-            _context.Entry(gameSession).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!GameSessionExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
+            return new GetSessionDto { 
+                Id = gameSession.Id,
+                CreatedAt = gameSession.CreatedAt,
+                Status = gameSession.Status,
+                Extensions = gameSession.Extensions.Select(extension => new GetExtensionDTO { Id = extension.Id, Name = extension.Name}).ToList(),
+                GameSessionUsers = gameSession.GameSessionUsers.Select(user => new GetUserMinDTO { Id = user.UserId, UserName= user.User.UserName }).ToList()
+            };
         }
 
         // POST: api/GameSessions
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
-        public async Task<ActionResult<GameSession>> PostGameSession(GameSession gameSession)
+        public async Task<ActionResult<GameSession>> PostGameSession(CreateSessionDto gameSession)
         {
-            _context.GameSessions.Add(gameSession);
+            var currentUser = await _userManager.FindByNameAsync(HttpContext.User.Identity.Name);
+            var session = new GameSession
+            {
+                CreatedAt = DateTime.Now,
+                Status = GameSessionStatus.Pending,
+                Extensions = _context.Extensions
+                .Where(item => gameSession.Extensions.Any(extensionItem => extensionItem == item.Id)).ToList(),
+                GameSessionUsers = new List<GameSessionUser>()
+            };
+            _context.GameSessions.Add(session);
+            await _context.SaveChangesAsync();
+            session.GameSessionUsers.Add(new GameSessionUser
+            {
+                GameSession = session,
+                GameSessionId = session.Id,
+                SessionRoles = GameSessionRoles.GameAdmin,
+                Status = GameSessionUserStatus.Accepted,
+                User = currentUser,
+                UserId = currentUser.Id
+            });
             await _context.SaveChangesAsync();
 
-            return CreatedAtAction("GetGameSession", new { id = gameSession.Id }, gameSession);
+            return CreatedAtAction("GetGameSession", new { id = session.Id }, session);
         }
 
         // DELETE: api/GameSessions/5
