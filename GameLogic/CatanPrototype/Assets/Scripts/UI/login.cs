2752456afdcf486f1df1087c7e9601149b7158f2
a-emanuel <79093616+a-emanuel@users.noreply.github.com>
2021-05-14 15:44:47 +0300

    api-ui-integration

--- GameLogic/CatanPrototype/Assets/Scripts/UI/login.cs
+++ GameLogic/CatanPrototype/Assets/Scripts/UI/login.cs
@@ -1,99 +1,77 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
 
 public class login : MonoBehaviour
 {
-    GameObject username, password;
-    // Start is called before the first frame update
+    GameObject usernameObj, passwordObj;
+
+
     public void getusername(GameObject username)
     {
-        this.username = username;
+        this.usernameObj = username;
     }
 
     public void getpassword(GameObject password)
     {
-        this.password = password;
+        this.passwordObj = password;
     }
+
     public void verifyLogin()
     {
-        if (username.GetComponent<UnityEngine.UI.InputField>().text!= "" && password.GetComponent<UnityEngine.UI.InputField>().text!= "")
+        string username = usernameObj.GetComponent<UnityEngine.UI.InputField>().text;
+        string password = passwordObj.GetComponent<UnityEngine.UI.InputField>().text;
+
+        if (username != "" && password != "")
         {
-            print(username.GetComponent<UnityEngine.UI.InputField>().text + " " + password.GetComponent<UnityEngine.UI.InputField>().text);
-            StartCoroutine(postLogin());
-            // StartCoroutine(getLogin());
-            // get when is correct or not si poate trece mai departe la MainMenu
+            StartCoroutine(postLogin(username, password));
         }
         else
         {
-            print("null here");
+            Debug.Log("Empty Field(s)");
         }
     }
-
-    public class LoginData
+    IEnumerator postLogin(string username, string password)
     {
-        // nume de parametrii din json
-        public string username;
-        public string password;
-        public string status;
-    }
+        string uri = "https://localhost:5001/api/Authenticate/login";
+        WWWForm form = new WWWForm();
+        form.AddField("username", username);
+        form.AddField("password", password);
 
-    IEnumerator getLogin()
-    {
-        print("a intrat aci");
-        string uri = "http://64.225.52.232:8921/alabala23";
-        UnityWebRequest www = UnityWebRequest.Get(uri);
-        yield return www.SendWebRequest();
-        
-        if (www.isNetworkError)
+        using(UnityWebRequest request = UnityWebRequest.Post(uri, form))
         {
-            print("erorar");
-        } else
-        {
-            try
+            yield return request.SendWebRequest();
+            if (request.result == UnityWebRequest.Result.ConnectionError || 
+            request.result == UnityWebRequest.Result.ProtocolError)
+                Debug.Log("POST Error");
+            else 
             {
-                // aici iau datele din json si vad fiecare parametru in parte
-                LoginData loginData = JsonUtility.FromJson<LoginData>(www.downloadHandler.text);
-                print(loginData.status);
-                // aici verifica daca statusul este ok si trece la mainmenu
-                //SceneManager.LoadScene("mainMenu");
-                // daca nu, afiseaza ceva mesaj
-            }
-            catch 
-            {
-                print("catch");
-            }
+                Debug.Log("POST OK");
+                long status = request.responseCode;
+                if (status == 200) 
+                {
+                    SceneManager.LoadScene(sceneName:"mainMenu");
+                }
+            }       
         }
-        
-    }
 
-    IEnumerator postLogin()
-    {
-        /////////////// put correct 
-        string uri = "https://jsonplaceholder.typicode.com/posts";
-        List<IMultipartFormSection> formData = new List<IMultipartFormSection>();
-        formData.Add(new MultipartFormDataSection("username="+username+"&parola="+password));
-
-        UnityWebRequest www = UnityWebRequest.Post(uri, formData);
-        yield return www.SendWebRequest();
-
-        if (www.isNetworkError)
-        {
-            print("eroare");
-        }
-        else
-        {
-            try
-            {
-                // aici iau datele din json si vad fiecare parametru in parte
-                //LoginData loginData = JsonUtility.FromJson<LoginData>(www.downloadHandler.text);
-                print(www.downloadHandler.text);
-            }
-            catch (System.Exception error)
-            {
-                print("catch" + error.ToString());
-            }
-        }
+        // string uri = "https://www.google.com";
+        // using(UnityWebRequest request = UnityWebRequest.Get(uri))
+        // {
+        //     yield return request.SendWebRequest();
+        //     if (request.result == UnityWebRequest.Result.ConnectionError || 
+        //     request.result == UnityWebRequest.Result.ProtocolError)
+        //         Debug.Log("Get Error");
+        //     else 
+        //     {
+        //         Debug.Log("Get OK");
+        //         long status = request.responseCode;
+        //         if (status == 200) 
+        //         {
+        //             SceneManager.LoadScene(sceneName:"mainMenu");
+        //         }
+        //     }       
+        // }
     }
 }
