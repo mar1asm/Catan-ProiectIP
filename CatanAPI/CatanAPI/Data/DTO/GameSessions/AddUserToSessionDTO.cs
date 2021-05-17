b7685f65cf4897038ddc38c5726f3744b8e5e771
Vlad BENCHEA <bencheavld@gmail.com>
2021-05-17 23:23:31 +0300

    Added DTO for inviting user to session

---
+++ CatanAPI/CatanAPI/Data/DTO/GameSessions/AddUserToSessionDTO.cs
@@ -0,0 +1,12 @@
+using CatanAPI.Models;
+using System;
+
+
+namespace CatanAPI.Data.DTO.GameSessionsDTO
+{
+    public class AddUserToSessionDTO
+    {
+        public string UserId { get; set; }
+    }
+
+}
