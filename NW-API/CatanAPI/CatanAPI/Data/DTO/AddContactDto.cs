5b6e641ddcd1f8df27ed74a8d84707b9c2bd5a71
Vlad BENCHEA <bencheavld@gmail.com>
2021-05-19 18:36:16 +0300

    Removed `NW-API/*` from master branch

--- NW-API/CatanAPI/CatanAPI/Data/DTO/AddContactDto.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-namespace CatanAPI.Data.DTO
-{
-    public class AddContactDto
-    {
-        [Required(ErrorMessage = "User Name is required")]
-        public string UserName { get; set; }
-    }
-}
