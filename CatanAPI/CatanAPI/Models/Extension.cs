3322bb32ba0146f3af48b80c368f56c75a869646
Vlad BENCHEA <bencheavld@gmail.com>
2021-05-21 17:32:43 +0300

    Added data field in Extension + migrations

--- CatanAPI/CatanAPI/Models/Extension.cs
+++ CatanAPI/CatanAPI/Models/Extension.cs
@@ -6,6 +6,7 @@ namespace CatanAPI.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Data { get; set; }
 
         public virtual ICollection<GameSession> GameSessions { get; set; }
     }
