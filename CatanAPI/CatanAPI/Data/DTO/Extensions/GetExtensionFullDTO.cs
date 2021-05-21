e1020621350e3dac8489a31af30ee331779186a4
Vlad BENCHEA <bencheavld@gmail.com>
2021-05-21 17:33:03 +0300

    Updated extension controller so it now processes the data field as well

---
+++ CatanAPI/CatanAPI/Data/DTO/Extensions/GetExtensionFullDTO.cs
@@ -0,0 +1,10 @@
+namespace CatanAPI.Data.DTO.ExtensionsDTO
+{
+    public class GetExtensionFullDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Data { get; set; }
+    }
+
+}
