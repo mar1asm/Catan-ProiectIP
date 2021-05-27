cda4dbf83e870be7d9c43f4f3ceb425f3051ec28
Spiridon Madalina <78969330+Madalina0303@users.noreply.github.com>
2021-05-27 12:02:45 +0300

    Add files via upload

---
+++ GameLogic/CatanPrototype/Assets/Scripts/GameLogic/BoardManager/Harbour.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Harbour 
+{
+    public float q0;
+    public float r0;
+    public float q1;
+    public float r1;
+    public string type;
+    public int raportx;
+    public int raporty;
+
+}
